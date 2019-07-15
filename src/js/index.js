cb6cb105ee5d39b5898bdf9ed022ae675dc588c8
jsephora <joey@sephoradesign.com>
2019-07-15 16:27:58 +1000

    index.js

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,4 @@ $('#alert').click(() => {
 });
 
 // Your jQuery code
-$('.table').on('hide.bs.collapse show.bs.collapse', e => {
-  $(e.target)
-    .prev()
-    .find('i:last-child')
-    .toggleClass('chevronUp chevronDown');
-});
+
