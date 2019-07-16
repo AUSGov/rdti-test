95e4f7d8df1f92ecdf3de502eeeb01564bed44eb
jsephora <joey@sephoradesign.com>
2019-07-16 10:12:40 +1000

    added jquery

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,11 @@ $('#alert').click(() => {
 
 // Your jQuery code
 
+$('.view').click(() => {
+  alert('This takes user to the referenced error on the form');
+});
+
+$('.allow').click(() => {
+  alert('This removes the warning');
+});
+
