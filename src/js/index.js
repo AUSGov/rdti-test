512e522fa2247626ccc3713a1c0d5fc2ad0f7e08
jsephora <joey@sephoradesign.com>
2020-02-17 15:29:25 +1100

    cant remember what i did

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,46 @@ $('.allow').click(() => {
   alert('This removes the warning');
 });
 
+$(function() {
+  $('#sidenav').load('sidenav.html');
+});
+
+/*$(function() {
+  $('#sidenav').on('click', '.nav', function(e) {
+    $(this)
+      .parents('#sidenav')
+      .find('.active')
+      .removeClass('active')
+      .end()
+      .end()
+      .addClass('active');
+  });
+});*/
+
+/*$(function() {
+  var url = window.location.href;
+  $('#sidenav li').each(function() {
+    if (url == this.href) {
+      $(this).addClass('active');
+    }
+  });
+});*/
+
+$(function() {
+  $('#sidenav a').click(function() {
+    $('a').removeClass('active');
+    $(this).addClass('active');
+  });
+});
+
+/*$(function() {
+  var current = location.pathname;
+  $('#sidenav li').each(function() {
+    var $this = $(this);
+    // if the current path is like this link, make it active
+    if ($this.attr('href').indexOf(current) !== -1) {
+      $this.parent().addClass('active');
+    }
+  });
+});*/
+
